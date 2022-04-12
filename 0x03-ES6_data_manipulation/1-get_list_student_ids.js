8d8a111177000c24ea10df50cfebe74a2adf7e60
dee-labs <divinechisom1995@gmail.com>
2022-04-12 22:37:29 +0100

    :rocket: pushed task 1 for checking

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,15 @@
+/*
+ * File: 1-get_list_student_ids.js
+ * Auth: Ukonu Divine Chisom
+ */
+
+/**
+ * getListStudentIds - Retrieves the id od student's list.
+ * @returns an array of ids from a list of object.
+ */
+export default function getListStudentIds(studentList){
+    if (studentList instanceof Array){
+        return studentList.map((item) => item.id);
+    }
+    return[];
+}
