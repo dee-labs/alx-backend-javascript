a3a50c5e8df67040128657105e226314bcd9c03f
dee-labs <divinechisom1995@gmail.com>
2022-04-15 13:32:22 +0100

    :memo: wrote documentation for 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,15 @@
+/*
+ * File: 4-update_grade_by_city.js
+ * Auth: Ukonu Divine Chisom
+ */
+
+/**
+ * updateStudentGradeByCity - Retrieves a list of students by city with their grades.
+ * @studentList: A list of students.
+ * @city: Student's location.
+ * @newGrades:Student's grade.
+ * Return: An array of students for a specific city with their new grade.
+ */
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
   return studentList
     .filter((item) => item.location === city)
